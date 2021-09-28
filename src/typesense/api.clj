da3ffd5073d81cea99b66bca1b3295e8c8fa16cd
Rune Nielsen <runenielsen@runbox.com>
2021-09-28 20:39:26 +0200

    removes util namespace

--- src/typesense/api.clj
+++ src/typesense/api.clj
@@ -1,58 +1,89 @@
 (ns typesense.api
-  (:require [typesense.utils :as utils]
-            [cheshire.core :as json]))
+  (:require [cheshire.core :as json]
+            [clojure.string :as str]
+            [clojure.walk :as walk]))
 
 (def ^:private api-key-header-name "X-TYPESENSE-API-KEY")
 
+(defn- collection-uri
+  "Returns the collection uri resource path."
+  ([settings]
+   (str (:uri settings) "/collections"))
+  ([settings collection-name]
+   (str (collection-uri settings) "/" collection-name)))
+
+(defn- document-uri
+  "Returns the document uri resource path."
+  [settings collection-name]
+  (str (:uri settings) "/collections/" collection-name "/documents"))
+
+(defn- build-query
+  "Convert param pairs into a valid query string."
+  [query-params]
+  (if (= (count query-params) 0)
+    ""
+    (->> query-params
+         walk/stringify-keys
+         (map #(str (key %) "=" (val %)))
+         (str/join \&)
+         (str "?"))))
+
+(defn- json-lines
+  "Take a vector of maps and returns json-line format."
+  [maps]
+  (reduce (fn [acc x] (str acc (json/generate-string x) "\n"))
+          ""
+          maps))
+
 (defn create-collection-req
   [settings schema]
-  {:uri (utils/collection-uri settings)
+  {:uri (collection-uri settings)
    :req {:headers {api-key-header-name (:key settings)
                    "Content-Type" "application/json"}
          :body (json/generate-string schema)}})
 
 (defn drop-collection-req
   [settings collection-name]
-  {:uri (utils/collection-uri settings collection-name)
+  {:uri (collection-uri settings collection-name)
    :req {:headers {api-key-header-name (:key settings)}}})
 
 (defn list-collections-req
   [settings]
-  {:uri (utils/collection-uri settings)
+  {:uri (collection-uri settings)
    :req {:headers {api-key-header-name (:key settings)}}})
 
 (defn retrieve-collection-req
   [settings collection-name]
-  {:uri (utils/collection-uri settings collection-name)
+  {:uri (collection-uri settings collection-name)
    :req {:headers {api-key-header-name (:key settings)}}})
 
 (defn create-document-req
   [settings collection-name document]
-  {:uri (utils/document-uri settings collection-name)
+  {:uri (document-uri settings collection-name)
    :req {:headers {api-key-header-name (:key settings)
                    "Content-Type" "application/json"}
          :body (json/generate-string document)}})
 
 (defn upsert-document-req
   [settings collection-name document]
-  {:uri (str (utils/document-uri settings collection-name) "?action=upsert")
+  {:uri (str (document-uri settings collection-name) "?action=upsert")
    :req {:headers {api-key-header-name (:key settings)
                    "Content-Type" "application/json"}
          :body (json/generate-string document)}})
 
 (defn retrieve-document-req
   [settings collection-name id]
-  {:uri (str (utils/document-uri settings collection-name) "/" id)
+  {:uri (str (document-uri settings collection-name) "/" id)
    :req {:headers {api-key-header-name (:key settings)}}})
 
 (defn delete-document-req
   [settings collection-name id]
-  {:uri (str (utils/document-uri settings collection-name) "/" id)
+  {:uri (str (document-uri settings collection-name) "/" id)
    :req {:headers {api-key-header-name (:key settings)}}})
 
 (defn update-document-req
   [settings collection-name id document]
-  {:uri (str (utils/document-uri settings collection-name) "/" id)
+  {:uri (str (document-uri settings collection-name) "/" id)
    :req {:headers {api-key-header-name (:key settings)
                    "Content-Type" "application/json"}
          :body (json/generate-string document)}})
@@ -64,29 +95,29 @@
                          documents
                          {}))
   ([settings collection-name documents parameters]
-   {:uri (str (utils/document-uri settings collection-name)
+   {:uri (str (document-uri settings collection-name)
               "/import"
-              (utils/build-query parameters))
+              (build-query parameters))
     :req {:headers {api-key-header-name (:key settings)
                     "Content-Type" "text/plain"}
-          :body (utils/json-lines documents)}}))
+          :body (json-lines documents)}}))
 
 (defn delete-documents-req
   [settings collection-name parameters]
-  {:uri (str (utils/document-uri settings collection-name)
-             (utils/build-query parameters))
+  {:uri (str (document-uri settings collection-name)
+             (build-query parameters))
    :req {:headers {api-key-header-name (:key settings)}}})
 
 (defn export-documents-req
   [settings collection-name parameters]
-  {:uri (str (utils/document-uri settings collection-name)
+  {:uri (str (document-uri settings collection-name)
              "/export"
-             (utils/build-query parameters))
+             (build-query parameters))
    :req {:headers {api-key-header-name (:key settings)}}})
 
 (defn search-req
   [settings collection-name parameters]
-  {:uri (str (utils/document-uri settings collection-name)
+  {:uri (str (document-uri settings collection-name)
              "/search"
-             (utils/build-query parameters))
+             (build-query parameters))
    :req {:headers {api-key-header-name (:key settings)}}})
--- src/typesense/core.clj
+++ src/typesense/core.clj
@@ -1,20 +1,35 @@
 (ns typesense.core
-  (:require [typesense.utils :as utils]
-            [typesense.api :as api]
-            [clj-http.client :as client]))
+  (:require [typesense.api :as api]
+            [clj-http.client :as client]
+            [cheshire.core :as json]
+            [clojure.string :as str]))
+
+(defn- handle-json-response
+  [response]
+  (-> response
+      :body
+      (json/parse-string true)))
+
+(defn- handle-jsonline-response
+  [response]
+  (->> response
+       :body
+       (str/split-lines)
+       (map #(json/parse-string % true))
+       (into [])))
 
 (defn create-collection
   "Create collection using the supplied collection schema."
   [settings schema]
   (let [req (api/create-collection-req settings schema)]
-    (utils/handle-json-response (client/post (:uri req) (:req req)))))
+    (handle-json-response (client/post (:uri req) (:req req)))))
 
 (defn drop-collection
   "Permanently drops a collection. This action cannot be undone.
   For large collections, this might have an impact on read latencies."
   [settings collection-name]
   (let [req (api/drop-collection-req settings collection-name)]
-    (utils/handle-json-response (client/delete (:uri req) (:req req)))))
+    (handle-json-response (client/delete (:uri req) (:req req)))))
 
 (defn list-collections
   "Returns a summary of all your collections.
@@ -22,44 +37,44 @@
   with the most recent collections appearing first."
   [settings]
   (let [req (api/list-collections-req settings)]
-    (utils/handle-json-response (client/get (:uri req) (:req req)))))
+    (handle-json-response (client/get (:uri req) (:req req)))))
 
 (defn retrieve-collection
   "Retrieves collection on collection name."
   [settings collection-name]
   (let [req (api/retrieve-collection-req settings collection-name)]
-    (utils/handle-json-response (client/get (:uri req) (:req req)))))
+    (handle-json-response (client/get (:uri req) (:req req)))))
 
 (defn create-document
   "Indexes the document."
   [settings collection-name document]
   (let [req (api/create-document-req settings collection-name document)]
-    (utils/handle-json-response (client/post (:uri req) (:req req)))))
+    (handle-json-response (client/post (:uri req) (:req req)))))
 
 (defn upsert-document
   "Indexes the document."
   [settings collection-name document]
   (let [req (api/upsert-document-req settings collection-name document)]
-    (utils/handle-json-response (client/post (:uri req) (:req req)))))
+    (handle-json-response (client/post (:uri req) (:req req)))))
 
 (defn retrieve-document
   "Retrieves the document on id in the specified collection."
   [settings collection-name id]
   (let [req (api/retrieve-document-req settings collection-name id)]
-    (utils/handle-json-response (client/get (:uri req) (:req req)))))
+    (handle-json-response (client/get (:uri req) (:req req)))))
 
 (defn delete-document
   "Deletes the document on id in the specified collection."
   [settings collection-name id]
   (let [req (api/delete-document-req settings collection-name id)]
-    (utils/handle-json-response (client/delete (:uri req) (:req req)))))
+    (handle-json-response (client/delete (:uri req) (:req req)))))
 
 (defn update-document
   "Update an individual document from a collection by using its id.
   The update can be partial"
   [settings collection-name id document]
   (let [req (api/update-document-req settings collection-name id document)]
-    (utils/handle-json-response (client/patch (:uri req) (:req req)))))
+    (handle-json-response (client/patch (:uri req) (:req req)))))
 
 (defn import-documents
   "Imports documents in the specified collection."
@@ -67,22 +82,22 @@
    (import-documents settings collection-name documents {}))
   ([settings collection-name documents parameters]
    (let [req (api/import-documents-req settings collection-name documents parameters)]
-     (utils/handle-jsonline-response (client/post (:uri req) (:req req))))))
+     (handle-jsonline-response (client/post (:uri req) (:req req))))))
 
 (defn delete-documents
   "Delete documents."
   [settings collection-name parameters]
   (let [req (api/delete-documents-req settings collection-name parameters)]
-    (utils/handle-json-response (client/delete (:uri req) (:req req)))))
+    (handle-json-response (client/delete (:uri req) (:req req)))))
 
 (defn export-documents
   "Exports documents in the specified collection."
   [settings collection-name parameters]
   (let [req (api/export-documents-req settings collection-name parameters)]
-    (utils/handle-jsonline-response (client/get (:uri req) (:req req)))))
+    (handle-jsonline-response (client/get (:uri req) (:req req)))))
 
 (defn search
   "Search for documents using the specified query parameters."
   [settings collection-name parameters]
   (let [req (api/search-req settings collection-name parameters)]
-    (utils/handle-jsonline-response (client/get (:uri req) (:req req)))))
+    (handle-jsonline-response (client/get (:uri req) (:req req)))))
