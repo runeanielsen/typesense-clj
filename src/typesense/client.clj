11347d73a5928fbc5d689d8e8a8f801e4de093c2
Rune Nielsen <runenielsen@runbox.com>
2021-09-28 21:12:17 +0200

    simplified req by destructuring

--- src/typesense/client.clj
+++ src/typesense/client.clj
@@ -23,83 +23,83 @@
 (defn create-collection!
   "Create collection using the supplied collection schema."
   [settings schema]
-  (let [req (api/create-collection-req settings schema)]
-    (handle-json-response (client/post (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/create-collection-req settings schema)]
+    (handle-json-response (client/post uri req))))
 
 (defn drop-collection!
   "Permanently drops a collection. This action cannot be undone.
   For large collections, this might have an impact on read latencies."
   [settings collection-name]
-  (let [req (api/drop-collection-req settings collection-name)]
-    (handle-json-response (client/delete (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/drop-collection-req settings collection-name)]
+    (handle-json-response (client/delete uri req))))
 
 (defn list-collections!
   "Returns a summary of all your collections.
   The collections are returned sorted by creation date,
   with the most recent collections appearing first."
   [settings]
-  (let [req (api/list-collections-req settings)]
-    (handle-json-response (client/get (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/list-collections-req settings)]
+    (handle-json-response (client/get uri req))))
 
 (defn retrieve-collection!
   "Retrieves collection on collection name."
   [settings collection-name]
-  (let [req (api/retrieve-collection-req settings collection-name)]
-    (handle-json-response (client/get (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/retrieve-collection-req settings collection-name)]
+    (handle-json-response (client/get uri req))))
 
 (defn create-document!
   "Indexes the document."
   [settings collection-name document]
-  (let [req (api/create-document-req settings collection-name document)]
-    (handle-json-response (client/post (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/create-document-req settings collection-name document)]
+    (handle-json-response (client/post uri req))))
 
 (defn upsert-document!
   "Indexes the document."
   [settings collection-name document]
-  (let [req (api/upsert-document-req settings collection-name document)]
-    (handle-json-response (client/post (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/upsert-document-req settings collection-name document)]
+    (handle-json-response (client/post uri req))))
 
 (defn retrieve-document!
   "Retrieves the document on id in the specified collection."
   [settings collection-name id]
-  (let [req (api/retrieve-document-req settings collection-name id)]
-    (handle-json-response (client/get (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/retrieve-document-req settings collection-name id)]
+    (handle-json-response (client/get uri req))))
 
 (defn delete-document!
   "Deletes the document on id in the specified collection."
   [settings collection-name id]
-  (let [req (api/delete-document-req settings collection-name id)]
-    (handle-json-response (client/delete (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/delete-document-req settings collection-name id)]
+    (handle-json-response (client/delete uri req))))
 
 (defn update-document!
   "Update an individual document from a collection by using its id.
   The update can be partial"
   [settings collection-name id document]
-  (let [req (api/update-document-req settings collection-name id document)]
-    (handle-json-response (client/patch (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/update-document-req settings collection-name id document)]
+    (handle-json-response (client/patch uri req))))
 
 (defn import-documents!
   "Imports documents in the specified collection."
   ([settings collection-name documents]
    (import-documents! settings collection-name documents {}))
   ([settings collection-name documents parameters]
-   (let [req (api/import-documents-req settings collection-name documents parameters)]
-     (handle-jsonline-response (client/post (:uri req) (:req req))))))
+   (let [{uri :uri req :req} (api/import-documents-req settings collection-name documents parameters)]
+     (handle-jsonline-response (client/post uri req)))))
 
 (defn delete-documents!
   "Delete documents."
   [settings collection-name parameters]
-  (let [req (api/delete-documents-req settings collection-name parameters)]
-    (handle-json-response (client/delete (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/delete-documents-req settings collection-name parameters)]
+    (handle-json-response (client/delete uri req))))
 
 (defn export-documents!
   "Exports documents in the specified collection."
   [settings collection-name parameters]
-  (let [req (api/export-documents-req settings collection-name parameters)]
-    (handle-jsonline-response (client/get (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/export-documents-req settings collection-name parameters)]
+    (handle-jsonline-response (client/get uri req))))
 
 (defn search!
   "Search for documents using the specified query parameters."
   [settings collection-name parameters]
-  (let [req (api/search-req settings collection-name parameters)]
-    (handle-jsonline-response (client/get (:uri req) (:req req)))))
+  (let [{uri :uri req :req} (api/search-req settings collection-name parameters)]
+    (handle-jsonline-response (client/get uri req))))
