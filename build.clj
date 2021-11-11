056310a90662f936e6f1737d82fd89f2eb7f7e77
Rune Nielsen <runenielsen@runbox.com>
2021-11-11 21:06:09 +0100

    adds ci and deploy to build

--- build.clj
+++ build.clj
@@ -1,22 +1,17 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
 
-(def lib 'runeanielsen/typesense-clj)
+(def lib 'runeanielsen/clojure-clj)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
-(def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn"}))
-(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn clean [_]
-  (b/delete {:path "target"}))
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/clean)
+      (bb/jar)))
 
-(defn jar [_]
-  (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version version
-                :basis basis
-                :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
-  (b/jar {:class-dir class-dir
-          :jar-file jar-file}))
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version version)
+      (bb/deploy)))
