7aa6360a6b1e0362fce784c116e6dbf99619ed9f
runeanielsen <runenielsen@runbox.com>
2024-06-09 06:16:46 +0200

    now generates a POM file and uses deps deploy instead of corfield build

--- build.clj
+++ build.clj
@@ -1,24 +1,61 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]))
+  (:require [clojure.string :as str]
+            [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as d]
+            [clojure.tools.deps :as t]))
 
 (def lib 'io.github.runeanielsen/typesense-clj)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn build "Build the JAR." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/clean)
-      (bb/jar)))
+;; delay to defer side effects (artifact downloads)
+(def basis (delay (b/create-basis {:project "deps.edn"})))
 
-(defn tests "Run the tests." [opts]
-  (-> opts
-      (bb/clean)
-      (bb/run-tests)))
+(def pom-template
+  [[:description "Clojure HTTP client for Typesense "]
+   [:url "https://github.com/runeanielsen/typesense-clj/"]
+   [:licenses
+    [:license
+     [:name "MIT"]
+     [:url "https://github.com/runeanielsen/typesense-clj/blob/master/LICENSE"]]]
+   [:developers
+    [:developer
+     [:name "Rune Andreas Nielsen"]]]])
 
-(defn deploy "Deploy the JAR to Clojars." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/clean)
-      (bb/jar)
-      (bb/deploy)))
+(defn- run-task [aliases]
+  (println "\nRunning task for" (str/join "," (map name aliases)))
+  (let [basis    (b/create-basis {:aliases aliases})
+        combined (t/combine-aliases basis aliases)
+        cmds     (b/java-command
+                  {:basis basis
+                   :main 'clojure.main
+                   :main-args (:main-opts combined)})
+        {:keys [exit]} (b/process cmds)]
+    (when-not (zero? exit) (throw (ex-info "Task failed" {})))))
+
+(defn clean []
+  (b/delete {:path "target"}))
+
+(defn build [_]
+  (clean)
+  (b/write-pom {:pom-data pom-template
+                :class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn tests [_]
+  (clean)
+  (run-task [:test :runner]))
+
+(defn deploy [_]
+  (clean)
+  (build)
+  (d/deploy {:installer :remote :artifact (b/resolve-path jar-file)
+             :pom-file (b/pom-path {:class-dir class-dir :lib lib})}))
